0cf1a44107ed52f263234a7e4711936176d40d98
thalesCastellani <62717114+thalesCastellani@users.noreply.github.com>
2022-08-15 21:14:09 -0300

    Implementando logica para reproducao de video

--- app/src/main/java/br/com/fiap/baseapp/TreinamentosActivity.kt
+++ app/src/main/java/br/com/fiap/baseapp/TreinamentosActivity.kt
@@ -1,11 +1,20 @@
 package br.com.fiap.baseapp
 
+import android.net.Uri
 import android.os.Bundle
-import android.widget.TextView
+import android.view.View
+import android.widget.ImageButton
+import android.widget.MediaController
+import android.widget.ProgressBar
+import android.widget.VideoView
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 
 class TreinamentosActivity : AppCompatActivity() {
+
+    private var uri: Uri? = null
+    private var isContinuously = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_treinamentos)
@@ -15,6 +24,48 @@ class TreinamentosActivity : AppCompatActivity() {
             actionBar?.setTitle("Treinamentos")
         }
 
-        val tvTreinamentos = findViewById<TextView>(R.id.tv_treinamentos)
+        val vv: VideoView = findViewById(R.id.vv)
+        val btnonce: ImageButton = findViewById(R.id.btnonce)
+        val btnconti: ImageButton = findViewById(R.id.btnconti)
+        val btnplay: ImageButton = findViewById(R.id.btnplay)
+        val btnstop: ImageButton = findViewById(R.id.btnstop)
+        val progrss: ProgressBar = findViewById(R.id.progrss)
+
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(vv)
+
+        val uriPath = "https://archive.org/download/WildlifeSampleVideo/Wildlife.mp4"
+
+        uri = Uri.parse(uriPath)
+
+        vv.setOnCompletionListener {
+            if (isContinuously) {
+                vv.start()
+            }
+        }
+
+        btnstop.setOnClickListener { vv.pause() }
+        btnplay.setOnClickListener { vv.start() }
+
+        btnonce.setOnClickListener {
+            isContinuously = false
+            progrss.visibility = View.VISIBLE
+            vv.setMediaController(mediaController)
+            vv.setVideoURI(uri)
+            vv.requestFocus()
+            vv.start()
+        }
+
+        btnconti.setOnClickListener {
+            isContinuously = true
+            progrss.visibility = View.VISIBLE
+            vv.setMediaController(mediaController)
+            vv.setVideoURI(uri)
+            vv.requestFocus()
+            vv.start()
+        }
+
+        vv.setOnPreparedListener { progrss.visibility = View.GONE }
+
     }
 }
